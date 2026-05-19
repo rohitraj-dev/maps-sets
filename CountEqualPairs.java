a5583fa846941314d89756cc858877fbf058222a
Rohit Raj <rajrohit7388@outlook.com>
2026-05-19 22:48:12 +0530

    Add files via upload

---
+++ CountEqualPairs.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+class CountEqualPairs
+{
+    long equalPairs(String s)
+    {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+        long pairs = 0;
+        for(char ch : map.keySet())
+        {
+            long freq = map.get(ch);
+            pairs += freq*freq;
+        }
+        return pairs;
+    }
+}
