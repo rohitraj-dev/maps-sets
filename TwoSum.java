b01d6e5943b778bd7c22fbbf6bdd675e4b019720
Rohit Raj <rajrohit7388@outlook.com>
2026-08-27 22:05:08 +0530

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+class Twosum
+{
+    boolean twoSum(int arr[], int target)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        for(int ele : arr)
+        {
+            int rem = target - ele;
+            if(set.contains(rem))
+                return true;
+            set.add(ele);
+        }
+        return false;
+    }
+}
